495f18af9b3c7dcc4d62a48489835cc8aafcdebc
alok-verma14 <154712574+alok-verma14@users.noreply.github.com>
2025-03-03 00:35:11 +0000

    number and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+
+const score = 100;
+const balance = new Number(234);
+// console.log(balance);
+
+const newNumber = new Number(256.456)
+const price = 7650000;
+// console.log(balance.toString());
+
+// console.log(newNumber.toPrecision(4));
+// console.log(price.toFixed(2));
+// console.log(price.toLocaleString('en-IN'));
+
+// +++++++++++++++++ Maths ++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(3.8));
+// console.log(Math.ceil(4.7));
+// console.log(Math.round(4.6));
+
+const max = 20;
+const min = 10;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
